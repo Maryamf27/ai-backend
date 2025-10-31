6851ffab45ae86d14115759694ce8f6eab23477e
Maryam Fatima <maryam27cs@gmail.com>
2025-10-31 09:47:52 +0500

    added api folder for vercel

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const app = require('./src/app')
 
 
 
-app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000')
-})
+// app.listen(3000, () => {
+//     console.log('Server is running on http://localhost:3000')
+// })
+
+// ✅ Don't use app.listen() here for Vercel
+module.exports = app;
